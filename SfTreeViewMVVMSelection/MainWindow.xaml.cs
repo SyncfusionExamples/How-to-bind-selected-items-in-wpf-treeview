9de39db86d7a0a4b7d2c67f622a388100794aee7
Susmitha Sundar <susmitha.sundar@syncfusion.com>
2020-07-09 18:25:56 +0530

    How to bind selected items in wpf treeview

---
+++ SfTreeViewMVVMSelection/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Syncfusion.Windows.Shared;
+
+namespace TreeViewMVVMselectionDemo
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
